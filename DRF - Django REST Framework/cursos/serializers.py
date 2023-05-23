25d7abf904666c37e9ec664ee7a35562ed44eef3
Pedro Feitosa <pedr.fts@gmail.com>
2023-05-23 13:42:52 -0300

    Inicio DRF

---
+++ DRF - Django REST Framework/cursos/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from .models import Curso, Avaliacao
+
+class AvaliacaoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        extra_kwargs = {
+            # email: privacidade, ou não vai se apresentado.
+            'email': {'write_only': True}
+        }
+        model = Avaliacao
+        fields = (
+            'id',
+            'cursos',
+            'nome',
+            'email',
+            'comentario',
+            'avaliacao',
+            'criacao',
+            'ativo'
+        )
+
+class CursoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Curso
+        fields = (
+            'id',
+            'titulo',
+            'url',
+            'criacao',
+            'ativo'
+        )
